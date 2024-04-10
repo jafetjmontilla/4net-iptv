1dfff301362b96c744c4ecc83f9ab39137ce2a0e
Jafet <febmerlib@gmail.com>
2024-04-10 22:52:04 +0000

    addeding cintrols

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
 import '@vidstack/react/player/styles/base.css';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react';
-import { isHLSProvider, MediaPlayer, MediaProvider, ScreenOrientationLockType, MediaCanPlayDetail, MediaCanPlayEvent, MediaProviderAdapter, MediaProviderChangeEvent } from '@vidstack/react';
+import { useEffect, useRef, useState } from 'react';
+import { isHLSProvider, MediaPlayer, MediaProvider, ScreenOrientationLockType, MediaCanPlayDetail, MediaCanPlayEvent, MediaProviderAdapter, MediaProviderChangeEvent, MediaPlayerInstance, Controls } from '@vidstack/react';
 import { Channel, channelsList } from "../utils/channels"
+import { FaAngleDown, FaAngleUp, FaCompress, FaExpand, FaMinus, FaPlus, FaPowerOff } from "react-icons/fa6";
+import { FaVolumeDown, FaVolumeMute } from "react-icons/fa";
+import { AnimatePresence, motion } from "framer-motion";
+import { RiHomeLine } from 'react-icons/ri';
+import { VolumeSli } from '../Components/VolumeSli';
 
 interface MyScreenOrientation extends ScreenOrientation {
   lock(orientation: ScreenOrientationLockType): Promise<void>;
 }
 
-
-const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
-  const router = useRouter()
   const [showVideo, setShowVideo] = useState<boolean>(false)
-  const [channel, setChannel] = useState<Channel>(channelsList[3])
-  // let player = useRef<MediaPlayerInstance>(null);
+  const [channel, setChannel] = useState<Channel>(channelsList[1])
+  const [showControl, setShowControl] = useState<boolean>(false);
+  const [closing, setClosing] = useState<boolean>(false);
+  const [isPc, setIsPc] = useState<boolean>(false);
+  const [mute, setMute] = useState<boolean>(false);
+  const [fullScreen, setFullScreen] = useState<boolean>(false);
+
+  let player = useRef<MediaPlayerInstance>(null);
+
+  let valirTimeout: any = null
 
   function onProviderChange(
     provider: MediaProviderAdapter | null,
@@ -28,53 +37,130 @@ export default function Home() {
     }
   }
 
-  // // We can listen for the `can-play` event to be notified when the player is ready.
   function onCanPlay(detail: MediaCanPlayDetail, nativeEvent: MediaCanPlayEvent) {
     console.log(10001, detail)
-    // ...
   }
 
   useEffect(() => {
-    const handlePopstate = () => {
-      console.log('El usuario presionó el botón de volver');
-      alert("algo1")
-      // Aquí puedes agregar la lógica que deseas ejecutar
+    const video: HTMLVideoElement | null = document.querySelector("video")
+    if (video) {
+      video.muted = mute
+    }
+  }, [mute])
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseLeave);
+
+    return () => {
+      clearTimeout(valirTimeout);
+      document.removeEventListener('mousemove', handleMouseLeave);
     };
-    const handleFull = (e: any) => {
-      if (!document.fullscreenElement) {
+  }, []);
+
+  useEffect(() => {
+    // Subscribe to state updates.
+    const video = document.querySelector("video")
+    return player.current!.subscribe((state: any) => {
+      console.log(channel.title)
+      console.log('is muted?', '->', state.muted);
+      console.log('is paused?', '->', state.paused);
+      console.log('is audio view?', '->', state.viewType);
+    });
+  }, []);
+
+  useEffect(() => {
+    const isPc = navigator?.userAgentData?.platform === "Windows"
+    setIsPc(isPc)
+    // const handlePopstate = () => {
+    //   console.log('El usuario presionó el botón de volver');
+    //   alert("algo1")
+    //   // Aquí puedes agregar la lógica que deseas ejecutar
+    // };
+    const handleFullChange = (e: any) => {
+      if (!document?.fullscreenElement) {
+        setFullScreen(false)
+      }
+    };
+    // window.addEventListener('popstate', handlePopstate);
+    window.addEventListener('fullscreenchange', handleFullChange);
+    return () => {
+      // window.removeEventListener('popstate', handlePopstate);
+      window.removeEventListener('fullscreenchange', handleFullChange);
+    };
+  }, []);
+
+  const handleSwithOff = async () => {
+    if (isPc) {
+      if (document?.fullscreenElement) {
+        document?.exitFullscreen()
+        setFullScreen(false)
+      }
+      setClosing(true)
+      setShowControl(false)
+      setShowVideo(false)
+      setTimeout(() => {
         const video: HTMLVideoElement | null = document.querySelector("video")
         if (video) {
           video.muted = true
         }
         setChannel({ id: 0, title: "", logo: "", src: "" })
+      }, 1500);
+    }
+    else {
+      setClosing(true)
+      setTimeout(() => {
+        if (document?.fullscreenElement) {
+          document?.exitFullscreen();
+        }
+        setShowControl(false)
         setShowVideo(false)
+      }, 400);
+      setTimeout(() => {
+        const video: HTMLVideoElement | null = document.querySelector("video")
+        if (video) {
+          video.muted = true
+        }
+        setChannel({ id: 0, title: "", logo: "", src: "" })
+      }, 1500);
+    }
+  }
+
+  const handleFullScreen = () => {
+    const container: any | null = document.getElementById("mediaplayer")
+    if (document?.fullscreenElement) {
+      if (document?.fullscreenElement) {
+        setFullScreen(false)
+        document?.exitFullscreen();
       }
-      // Aquí puedes agregar la lógica que deseas ejecutar
-    };
-    // Agrega el evento al cargar el componente
-    window.addEventListener('popstate', handlePopstate);
-    window.addEventListener('fullscreenchange', handleFull);
-    // Limpia el evento al desmontar el componente
-    return () => {
-      window.removeEventListener('popstate', handlePopstate);
-      window.removeEventListener('fullscreenchange', handleFull);
-    };
-  }, []);
+    } else {
+      if (container?.requestFullscreen) {
+        setFullScreen(true)
+        container?.requestFullscreen({ navigationUI: "hide" });
+        (screen.orientation as MyScreenOrientation).lock('landscape')
+          .then()
+          .catch((error: any) =>
+            console.log(10004, error)
+          )
+      }
+    }
+  }
+
+  useEffect(() => {
+    console.log({ closing })
+  }, [closing])
 
   const handleSwithOn = () => {
-    //setChannel(channel.src)
+    setClosing(false)
     setTimeout(() => {
       setShowVideo(true)
-      setChannel(channelsList[3])
+      setChannel(channelsList[12])
       setTimeout(() => {
         const video: HTMLVideoElement | null = document.querySelector("video")
         const container: any | null = document.getElementById("mediaplayer")
         if (video) {
-          console.log("-------------->", 544411)
           video.muted = false
           video.volume = 0.5
-          console.log(video?.volume)
-          if (container?.requestFullscreen) {
+          if (container?.requestFullscreen && !isPc) {
             container?.requestFullscreen({ navigationUI: "hide" });
             (screen.orientation as MyScreenOrientation).lock('landscape')
               .then()
@@ -83,48 +169,138 @@ export default function Home() {
               )
           }
         }
-      }, 100);
-    }, 100)
-    setTimeout(() => {
-      setChannel(channelsList[11])
-      const video: HTMLVideoElement | null = document.querySelector("video")
-      setTimeout(() => {
-        if (video) {
-          video.muted = false
-        }
-      }, 100);
-    }, 4000);
+      }, 500);
+    }, 500)
   }
 
-  useEffect(() => {
-    console.log("-------------->", { channel })
-  }, [showVideo])
-
+  const handleMouseLeave = () => {
+    if (!showControl) {
+      setShowControl(true);
+    }
+    clearTimeout(valirTimeout)
+    const ID = setTimeout(() => {
+      setShowControl(false);
+      valirTimeout = null
+    }, 3500);
+    valirTimeout = ID
+  };
 
   return (
-    <main className={`bg-black flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}    >
-      <div onClick={handleSwithOn} className="w-10 h-10 bg-red-400">
-        hola  mundo
-      </div>
-      <Image style={{ objectFit: 'cover' }} height={40} width={100} alt={channel.title} src={channel.logo} />
-      <div className={`w-[400] bg-red`}>
-        {/* <button className='w-10 h-4 bg-green-300 text-white' type='button' onClick={handleSwithOn}>sound</button> */}
-        <div className=' w-[400px]'>
+    <main className={`bg-black flex min-h-screen flex-col items-center justify-center vertical-content`}    >
+      <AnimatePresence  >
+        {!showVideo && <motion.div
+          key={"1"}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: closing ? 1 : 0, duration: 0.4 } }}
+          exit={{ opacity: 0, transition: { duration: 0.2 } }}
+          className='absolute right-10 bottom-10'
+        >
+          <div className='flex flex-col items-center'>
+            <div className='items-center'>
+              <div onClick={handleSwithOn} className="inline-flex cursor-pointer p-8 bg-slate-900 rounded-full hover:scale-110 hover:bg-slate-800">
+                <FaPowerOff className='w-20 h-20 text-white ' />
+              </div>
+            </div>
+            <Image style={{ objectFit: 'cover' }} height={40} width={300} alt={channel.title} src={"/4netBlancoGradient.png"} />
+          </div>
+        </motion.div>}
+        <motion.div
+          key={"2"}
+          initial={false}
+          animate={{
+            width: !showVideo ? 0 : "100%",
+            transition: { delay: 0.5, duration: 0.4 }
+          }}
+          exit={{ opacity: 0, transition: { duration: 0.2 } }}
+          className='absolute'
+        >
           <MediaPlayer
+            ref={player}
             id='mediaplayer'
             muted={true}
             autoPlay={true}
-            className={`${!showVideo && "!hidden"} aspect-video bg-slate-900 text-white font-sans overflow-hidden rounded-md ring-media-focus data-[focus]:ring-4`}
+            className={`aspect-video bg-black text-white font-sans overflow-hidden rounded-md ring-media-focus data-[focus]:ring-4`}
             // title="Sprite Fight"
             src={[{ src: channel.src }]}
             crossOrigin={true}
             playsInline={true}
+            title={channel.title}
+            onProviderChange={onProviderChange}
+            onCanPlay={onCanPlay}
           >
             <MediaProvider />
+            <div className={`absolute inset-0 z-10 flex h-full w-full flex-col justify-center items-end -translate-x-7 md:-translate-x-20`}>
+              <AnimatePresence >
+                {showControl &&
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 0.8, transition: { duration: 0.4 } }}
+                    exit={{ opacity: 0.5, transition: { delay: 0.4, duration: 0.2 } }}
+                  >
+                    <AnimatePresence >
+                      {!closing &&
+                        <motion.div
+                          initial={{ opacity: 0 }}
+                          animate={{ opacity: 0.8, transition: { duration: 0.4 } }}
+                          exit={{ opacity: 0, transition: { duration: 0.15 } }}
+                        >
+                          <div className={`bg-black p-2 rounded-3xl *opacity-80 scale-[65%] md:scale-100`}>
+                            <div className="w-[265px] h-[478px] rounded-3xl border-white border-8 flex flex-col" >
+                              <div className="w-full h-[85%] flex">
+                                <div className="w-1/3 h-full flex flex-col items-center justify-between py-8">
+                                  <div onClick={() => { handleSwithOff() }} className="w-[60px] h-[60px]  rounded-full border-4 border-white flex justify-center items-center cursor-pointer" ><FaPowerOff className="w-8 h-8 hover:scale-110" /></div>
 
+                                  <div className="w-[56px] md:w-[52px] h-[138px]  rounded-full border-4 border-white flex flex-col justify-center items-center relative" >
+                                    <div className="w-full h-1/3 flex justify-center items-center cursor-pointer hover:scale-110"><FaPlus className="w-6 h-6" /></div>
+                                    <div className="w-full h-1/3 flex justify-center items-center text-lg">Vol</div>
+                                    <div className="w-full h-1/3 flex justify-center items-center cursor-pointer hover:scale-110"><FaMinus className="w-6 h-6" /></div>
+                                  </div>
+                                  <div onClick={() => { setMute(!mute) }} className="w-[60px] h-[60px]  rounded-full border-4 border-white flex justify-center items-center cursor-pointer" >
+                                    {mute
+                                      ? <FaVolumeMute className="w-8 h-8 hover:scale-110" />
+                                      : <FaVolumeDown className="w-8 h-8 hover:scale-110" />
+                                    }
+                                  </div>
+                                </div>
+                                <div className="w-1/3 h-full flex justify-center items-center">
+                                  <div className="w-[60px] h-[60px]  rounded-full border-4 border-white flex justify-center items-center cursor-pointer" ><RiHomeLine className="w-8 h-8 hover:scale-110" /></div>
+                                </div>
+                                <div className="w-1/3 h-full flex flex-col items-center justify-between py-8">
+                                  {isPc
+                                    ? <div onClick={() => { handleFullScreen() }} className="w-[60px] h-[60px]  rounded-full border-4 border-white flex justify-center items-center cursor-pointer"  >
+                                      {fullScreen
+                                        ? <FaCompress className="w-8 h-8 hover:scale-110" />
+                                        : <FaExpand className="w-8 h-8 hover:scale-110" />}
+                                    </div>
+                                    : <div className="w-[60px] h-[60px]" />
+                                  }
+
+                                  <div className="w-[56px] md:w-[52px] h-[138px]  rounded-full border-4 border-white flex flex-col justify-center items-center" >
+                                    <div className="w-full h-1/3 flex justify-center items-center cursor-pointer hover:scale-110"><FaAngleUp className="w-6 h-6" /></div>
+                                    <div className="w-full h-1/3 flex justify-center items-center text-lg">Ch</div>
+                                    <div className="w-full h-1/3 flex justify-center items-center cursor-pointer hover:scale-110"><FaAngleDown className="w-6 h-6" /></div>
+                                  </div>
+                                  <div className="w-[60px] h-[60px]  rounded-full border-4 border-white flex justify-center items-center cursor-pointer" >
+                                    <div className='w-9 h-6 rounded-[4px] border-[3px] border-white flex justify-end items-end p-[2px] hover:scale-110'>
+                                      <div className='bg-white w-4 h-2.5 rounded-[2px] border-white' />
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className='w-full flex-1 flex items-center justify-center pb-6'>
+                                <Image style={{ objectFit: 'cover' }} height={10} width={75} alt={channel.title} src={"/4netBlancoGradient.png"} />
+                              </div>
+                            </div>
+                          </div>
+                        </motion.div>
+                      }
+                    </AnimatePresence>
+                  </motion.div>
+                }
+              </AnimatePresence>
+            </div>
           </MediaPlayer>
-        </div>
-        <style>{`
+          <style>{`
       video::-webkit-media-controls {
         display: none;
       }
@@ -135,7 +311,8 @@ export default function Home() {
         display: none !important;
       }
       `}</style>
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </main >
   )
 }
--- utils/channels.ts
+++ utils/channels.ts
@@ -5,63 +5,70 @@ export interface Channel {
   title: string
   logo: string
   src: string
+  showMenu?: boolean
 }
 
 
 export const channelsList: Channel[] = [
   {
     id: 0,
+    title: "intro",
+    logo: "/channels/antv.webp",
+    showMenu: true,
+    src: "/channels/intro.mp4"
+  },
+  {
+    id: 1,
     title: "antv",
     logo: "/channels/antv.webp",
     src: "https://airtek.tv/livestream/ANTV.m3u8"
   },
   {
-    id: 1,
+    id: 2,
     title: "canal 11",
     logo: "/channels/canal11.webp",
-    src: "https://airtek.tv/livestream/CANAL_11.m3u8"
-  },
-  {
-    id: 2,
-    title: "canal i",
-    logo: "/channels/canali.webp",
-    src: "https://airtek.tv/livestream/CANAL_I.m3u8"
+    src: "https://airtek.tv/livestream/CANAL_11.m3u8"//contactar
   },
   {
     id: 3,
-    title: "colombeia",
-    logo: "/channels/colombeia.webp",
-    src: "https://airtek.tv/livestream/COLOMBEIA.m3u8"
+    title: "canal i",
+    logo: "/channels/canali.webp",
+    src: "https://airtek.tv/livestream/CANAL_I.m3u8"//contactar
   },
   {
     id: 4,
+    title: "colombeia",
+    logo: "/channels/colombeia.webp",
+    //src:"https://live-c.cf.dmcdn.net/sec2(YLDQhjis-kgtEwIkXiJ3-Yaxxu3tfRY64joFXXBL0RyYI6toVX6dDbUxUyMGZA1sOn5V6Z4CiuB_syE-6ot4_oPzHDKOLkW6PnP52QJMjFg)/dm/3/x8sp8x8/s/live-2.m3u8"
+    src: "https://airtek.tv/livestream/COLOMBEIA.m3u8"
+  },
+  {
+    id: 5,
     title: "conciencia tv",
     logo: "/channels/conciencia_tv.webp",
     src: "https://airtek.tv/livestream/CONCIENCIA_TV.m3u8"
   },
   {
-    id: 5,
+    id: 6,
     title: "globovision",
     logo: "/channels/globovision.webp",
     src: "https://airtek.tv/livestream/Globovision.m3u8"
   },
   {
-    id: 6,
+    id: 7,
     title: "telesur",
     logo: "/channels/telesur.webp",
-    src: "https://airtek.tv/livestream/TELESUR.m3u8"
-  },
-  {
-    id: 7,
-    title: "televen",
-    logo: "/channels/televen.webp",
-    src: "https://airtek.tv/livestream/TELEVEN.m3u8"
+        showMenu: true,
+src: "https://cdnesmain.telesur.ultrabase.net/mbliveMain/hd/chunklist.m3u8"
+    //src: "https://airtek.tv/livestream/TELESUR.m3u8"
   },
   {
     id: 8,
-    title: "tlt",
-    logo: "/channels/tlt.webp",
-    src: "https://airtek.tv/livestream/TLT.m3u8"
+    title: "televen",
+    logo: "/channels/televen.webp",
+    showMenu: true,
+    src: "https://mediablocks-sec.global.ssl.fastly.net/10255/live/hls/televen/index.m3u8?hdnts=data%3Dip%3D255.223.177.14%2Cid%3DTLV-GUEST%2Cpid%3D255%2Cdid%3DN%2FA%2Csid%3DWBS~acl%3D%2F10255%2Flive%2Fhls%2Fteleven%2F%2A~exp%3D1712686763~hmac%3D5849004cd3d0f8acfab4f39a6e6e088cbc2ef6b6efdc894f45a51ca6601b1a63&AV_AID=STPLX_AID&AV_IDFAMD5=STPLX_IDFAMD5&AV_CATEGORY=IAB1-7&AVC_TIME=STPLX_TM&AV_UPAL=STPLX_UPAL&AV_CCPA=1YYY&AV_TITLE=Televen+HD&AV_APPSTOREURL=STPLX_ASTRURL&AV_SECURED=1&AV_APPPKGNAME=STPLX_APKGN&AV_CHANNEL_NAME=Televen+HD&AV_DNT=0&AVC_INVIEWPERC=100&AV_URL=https%3A%2F%2FSTPLX_DOMAIN%2Fplayer%2Ftv%3FchannelId%3D20000841%26categoryId%3D0%26channelNumber%3D1&AV_VIDEOID=20000841&AV_APPVERS=STPLX_AVER&AV_CONTENT_TYPE=LIVE&AV_AIDMD5=STPLX_AIDMD5&AV_AIDSHA1=STPLX_AIDSHA1&AV_IDFASHA1=STPLX_IDFASHA1&AV_MAKE=STPLX_MK&AVC_DATE=STPLX_DT&AV_IDFA=STPLX_IDFA&AV_APPNAME=STPLX_ANM&AV_DTRACK=1&AV_VIDEOURL=https%3A%2F%2FSTPLX_DOMAIN%2Fplayer%2Ftv%3FchannelId%3D20000841%26categoryId%3D0%26channelNumber%3D1&AV_SITE_CATEGORY=IAB1-7&AV_LANGUAGE=Sp&AV_MODEL=STPLX_MDL&AVC_INVIEW=1"
+    //src: "https://airtek.tv/livestream/TELEVEN.m3u8"
   },
   {
     id: 9,
@@ -73,19 +80,22 @@ export const channelsList: Channel[] = [
     id: 10,
     title: "tves",
     logo: "/channels/tves.webp",
+    //src:"https://edge1o.live.opencaster.com/caster/erCkqDzdGzpj.m3u8"
     src: "https://airtek.tv/livestream/TVES.m3u8"
   },
   {
     id: 11,
     title: "tvfanb",
     logo: "/channels/tvfanb.webp",
-    src: "https://airtek.tv/livestream/TV_FANB.m3u8"
+    src: "https://airtek.tv/livestream/TV_FANB.m3u8"//contactar
   },
   {
-    id: 12,
+    id: 12, 
     title: "venevision",
     logo: "/channels/venevision.webp",
-    src: "https://airtek.tv/livestream/Venevision.m3u8"
+    showMenu: true,
+    src:"https://venevision.akamaized.net/hls/live/2098814/VENEVISION/master.m3u8"
+    //src: "https://airtek.tv/livestream/Venevision.m3u8"
   },
   {
     id: 13,
@@ -97,8 +107,10 @@ export const channelsList: Channel[] = [
     id: 14,
     title: "vtv",
     logo: "/channels/vtv.webp",
+    //src:"https://live-c.cf.dmcdn.net/sec2(O4w2DM2P2OwimOVf7nWQOcEHPyY8K0KoiCzF_a5oRpYfbdyonce9ZHI96ay1nU29026lmMylZabf04HaB5YmvK1eL4Fuh2yHiyQJ68sDM8E)/dm/3/x8rtkvs/s/live-1.m3u8"
     src: "https://airtek.tv/livestream/VTV.m3u8"
   },
+
 ]
 //   "antv",
 //   "canal_i",
