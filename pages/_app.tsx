65b63dd63ba726fabd59373f7daa3ac94007d258
Jafet <febmerlib@gmail.com>
2024-05-18 20:07:14 +0000

    added noindex robot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="description" content="¡IPTV! Fournet." />
+        <meta name="robots" content="noindex"></meta>
       </Head>
       <Component {...pageProps} />
       <style jsx global>
